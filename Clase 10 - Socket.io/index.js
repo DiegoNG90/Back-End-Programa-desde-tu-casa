501f99c199f8bdbab5a3e8ef723a7f8b8341bae2
Diego <diego.nicolas.gandara@gmail.com>
2020-12-03 20:29:29 -0300

    En las lineas 1 a 5 muestro una nueva forma de configurar express y app, es decir, el servidor. Luego, en la linea 12, trabajamos con el metodo sendFile para la peiticion nos redirija al archivo que creamos en el commit anterior: index.html

--- Clase 10 - Socket.io/index.js
+++ Clase 10 - Socket.io/index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const app = express();
+//Ésta manera es la misma o equivalente a:
+// const express = require('express);
+//const app = express():
+const app = require('express')();
+
 //Al servidor que creamos le vamos a anexar OTROS protocolos: TCP y HTTP
 const http = require('http').Server(app);
 //requerimos io y ejecutamos http:
 const io = require('socket.io')(http);
 
 app.get('/', (req,res) =>{
-    res.send(`<h1>Hola!</h1>`)
+    res.sendFile(__dirname+'/index.html')
 })
-app.listen(8080, () => {
+http.listen(8080, () => {
     console.log("Escuchando el servidor 8080");
 })
