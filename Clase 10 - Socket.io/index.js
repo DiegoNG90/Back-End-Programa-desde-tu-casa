a1b1a627aa424555ca1674c149951a3c9558778c
Diego <diego.nicolas.gandara@gmail.com>
2020-12-03 21:33:53 -0300

    Creamos la carpeta public, donde se alojaran los archivos del front. Entonces, en el index.html, integramos el script.js que tendrá la variable socket que permite la conexion desde el front. También integramos bootstrap. Ademas, configuramos io para la conexion y la desconexion

--- Clase 10 - Socket.io/index.js
+++ Clase 10 - Socket.io/index.js
@@ -1,20 +1,25 @@
+const express = require('express');
+const app = express();
 //Ésta manera es la misma o equivalente a:
-// const express = require('express);
-//const app = express():
-const app = require('express')();
+// const app = require('express')();
 
 //Al servidor que creamos le vamos a anexar OTROS protocolos: TCP y HTTP
 const http = require('http').Server(app);
 //requerimos io y ejecutamos http:
 const io = require('socket.io')(http);
 
-app.get('/', (req,res) =>{
-    res.sendFile(__dirname+'/index.html')
-})
+// app.get('/', (req,res) =>{
+//     res.sendFile(__dirname+'/index.html')
+// })
+
+app.use(express.static('public'));
 
 //Trabajamos con io.on
 io.on('connection', (socket) => {
-    console.log('usuario conectado');
+    console.log('usuario conectado. ID: ' + socket.id);
+    socket.on('disconnect', () => {
+        console.log("usuario se ha desconectado");
+    })
 })
 
 http.listen(8080, () => {
