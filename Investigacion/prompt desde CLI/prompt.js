983c9a9817e2c6bbcb3b4fcbd77d1a5f5e2485cc
Diego <diego.nicolas.gandara@gmail.com>
2020-12-09 17:23:27 -0300

    Creo paquete.json e instalo la libreria de inquirer. Es otra manera de hacer lo mismo, pero en vez de usar process, usamos una libreria externa

--- Investigacion/prompt desde CLI/prompt.js
+++ Investigacion/prompt desde CLI/prompt.js
@@ -1,10 +1,26 @@
 //Cómo crear un prompt desde Node, dado que nativamente, Node no acepta el método prompt().
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
   
-readline.question(`What's your name?`, name => {
-    console.log(`Hi ${name}!`)
-    readline.close()
+// readline.question(`What's your name?`, name => {
+//     console.log(`Hi ${name}!`)
+//     readline.close()
+// })
+
+/*Ahora supongamos que queremos meter de input una contrasela. Tenemos que trabajar con otro módulo, llamado inquirer */
+const inquirer = require('inquirer')
+
+let question = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What's your last name?"
+    }
+];
+
+inquirer.prompt(question)
+.then(answer => {
+    console.log(`Hi, ${answer['name']}!`);
 })
