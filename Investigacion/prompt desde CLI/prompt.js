6c4cf9fe0290fc6ebf86c0ee9091a2afba09f424
Diego <diego.nicolas.gandara@gmail.com>
2020-12-09 17:37:20 -0300

    Se instala paquete readline-sync y probamos ocultar el input dado por el usuario con el comando readlineSyinc, a traves de su propiedad 0hideEchoBack true

--- Investigacion/prompt desde CLI/prompt.js
+++ Investigacion/prompt desde CLI/prompt.js
@@ -10,17 +10,31 @@
 // })
 
 /*Veamos una manera de hacer lo mismo */
-const inquirer = require('inquirer')
+// const inquirer = require('inquirer')
 
-let question = [
-    {
-        type: 'input',
-        name: 'name',
-        message: "What's your last name?"
-    }
-];
+// let question = [
+//     {
+//         type: 'input',
+//         name: 'name',
+//         message: "What's your last name?"
+//     }
+// ];
 
-inquirer.prompt(question)
-.then(answer => {
-    console.log(`Hi, ${answer['name']}!`);
-})
+// inquirer.prompt(question)
+// .then(answer => {
+//     console.log(`Hi, ${answer['name']}!`);
+// })
+
+/* Ahora sí: Si queremos ocultar el contenido que el usuario da como respuesta, vamos a ver como, con el uso del objeto readlineSync y su propiedad hideEchoBack*/
+
+var readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+ 
+// Handle the secret text (e.g. password).
+var favFood = readlineSync.question('What is your favorite food? ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+});
+console.log('Oh, ' + userName + ' loves ' + favFood + '!');
