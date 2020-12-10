d443283b4a6817e8fab6fe023a9836d38ddc8ec6
Diego <diego.nicolas.gandara@gmail.com>
2020-12-10 20:03:11 -0300

    creo servidor desde http

--- Clase 11 - Continuacion Socket IO y chat Room/index.js
+++ Clase 11 - Continuacion Socket IO y chat Room/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const { Http2ServerRequest } = require('http2');
 const app = express();
-const htttp = require('http').Server(app);
+const http = require('http').Server(app);
 const io = require('socket.io');
 
+http.listen(8080, () => {
+    console.log("Puerto escuchando en 8080");
+})
