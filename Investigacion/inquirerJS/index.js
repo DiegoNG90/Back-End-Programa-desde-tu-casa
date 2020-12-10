c7758e2014269a1c1eae216676ae195a3e5e6312
Diego <diego.nicolas.gandara@gmail.com>
2020-12-10 00:49:07 -0300

    Agrego complejidad agregando una tercera pregunta. Tengo que cambiar la estructura ternaria por flow/branch control if/else

--- Investigacion/inquirerJS/index.js
+++ Investigacion/inquirerJS/index.js
@@ -14,6 +14,11 @@ var questions = [
       type: 'confirm',
       name: 'yes/no',
       message: 'Are you 18 or older?: Yes/No'
+  },
+  {
+    type: 'confirm',
+    name: 'band',
+    message: 'Are you going to see the band?'
   }
 ]
 //Programa para acceso a un bar
@@ -21,8 +26,23 @@ inquirer.prompt(questions).then(answers => {
   console.log(`Hi ${answers['name']}!`);
   
   // imprimiendo answers, obtengo un objeto. Con ésta línea descifré la siguiente.
-  // console.log(answers);
-
+  
   //Operador ternario: si es mayor, bienvenido, sino, a tu casa
-  (answers['yes/no'])? console.log(`${answers['name']}, you are welcome in the club`) : console.log("You are a child, go home!");;
+  if (answers['yes/no']) {
+      console.log(`${answers['name']}, you are welcome in the club`);
+      if (answers.band === true) {
+        console.log(`${answers.name}, you have to pay $20 to see the band`);
+      }else{
+        console.log("Go foward, then left and you'll get to the bar");
+      }
+      
+  } else{
+  console.log("You are a child, go home!");
+  }
+
+  // (answers['yes/no'])? console.log(`${answers['name']}, you are welcome in the club`) : console.log("You are a child, go home!");
+
+  // //operador ternario: si viene a la banda: paga $20 extra. Sino, "al fondo a la izquierda"
+  // (answers.band === true)? console.log(`${answer.name}, you have to pay $20 to see the band`) : console.log("Go foward, then left and you'll get to the bar");
+  // console.log(answers);
 });
