fa00cc9fd6a917be69ec8c5f754e506935d57372
Diego <diego.nicolas.gandara@gmail.com>
2020-12-10 20:10:53 -0300

    Creamos carpeta public, con index.html dentro y lo levantamos desde index.js usando app.use y seteandolo para que los archivos estaticos sean levantados por express desde la carpeta public. También configuramos el io del lado del back

--- Clase 11 - Continuacion Socket IO y chat Room/index.js
+++ Clase 11 - Continuacion Socket IO y chat Room/index.js
@@ -4,6 +4,18 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io');
 
+//app.use(express.static(path.join(__dirname+'public'))); INVESTIGAR
+//Con ésta línea, pedimos a express que use la carpeta public para ir a buscar los archivos estaticos
+app.use(express.static('public'));
+
+
+//Configuramos el io desde el lado del back
+io.on('connection', (socket) => {
+    console.log("Un usuario se ha logueado");
+})
+
+
+//Levanto el Server
 http.listen(8080, () => {
     console.log("Puerto escuchando en 8080");
 })
