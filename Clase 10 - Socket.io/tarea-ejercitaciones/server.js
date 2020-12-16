2eaae40c29f7c58d40087cc6e6c5ab8e070826a9
Diego <diego.nicolas.gandara@gmail.com>
2020-12-16 13:59:36 -0300

    Seteo el puerto para que escuche el puerto 8080. En éste punto saltó un bug en la instanciación de socket! (linea 4). Arreglado

--- Clase 10 - Socket.io/tarea-ejercitaciones/server.js
+++ Clase 10 - Socket.io/tarea-ejercitaciones/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app)
-const io = require('socket-io')(http)
+const io = require('socket.io')(http)
+
+http.listen(8080, ()=> {
+    console.log("Escuchando en puerto 8080");
+})
