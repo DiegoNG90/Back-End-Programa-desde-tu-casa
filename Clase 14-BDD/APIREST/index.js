d5ccde33d4681e66d6766b5e0e1b02abf90c359c
Diego <diego.nicolas.gandara@gmail.com>
2021-01-05 22:08:41 -0300

    Creamos la conexion y chequeamos por consola si funciona o no la conexion a MYSQL

--- Clase 14-BDD/APIREST/index.js
+++ Clase 14-BDD/APIREST/index.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+//Creamos la conexion a la base de datos
+const connectionDb = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "PDC"
+})
+
+//Chequear si funciona la conexion
+connectionDb.connect( (err) => {
+    if (err){
+        throw err;
+    }
+    else{
+        console.log("Conexion exitosa");
+    }
+} )
 
 app.listen(8080, ()=> {
     console.log("Servidor escucuando en puerto 8080");
