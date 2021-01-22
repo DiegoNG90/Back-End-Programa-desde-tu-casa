7c689d0f168c59a0cd648cb919f8ed6574f1b7ed
Diego <diego.nicolas.gandara@gmail.com>
2021-01-21 22:40:43 -0300

    Instalo el paquete cors y lo requiero y ejecuto en la index/server de la API

--- Clase 15 - API modularizacion/APIREST/index.js
+++ Clase 15 - API modularizacion/APIREST/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const alumnosRouter = require('./routes/alumnos.js')
 
-
 //Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/gestoralumnos', alumnosRouter)
